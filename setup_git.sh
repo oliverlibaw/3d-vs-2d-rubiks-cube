d7ebfb6ab5a9e1431bffb29dcf9354cfacfd07f0
oliverlibaw <oliver_libaw@yahoo.com>
2025-11-08 07:59:39 -0800

    Initial app with 2d and 3D representations of a Rubik's Cube, with auto-solving for both versions.

---
+++ setup_git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git init
+git remote add origin https://github.com/oliverlibaw/3d-vs-2d-rubiks-cube.git
+git add .
+git commit -m "feat: Implement Rubik's cube interaction and solving"
+git push -u origin main
